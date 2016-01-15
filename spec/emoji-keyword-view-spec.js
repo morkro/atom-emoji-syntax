ac653a4d348682ffc9be0b61874bcd3cee53252a
Moritz Kröger <write@morkro.de>
2016-01-15 23:30:42 +0100

    🔧 improved testing

--- spec/emoji-keyword-view-spec.js
+++ spec/emoji-keyword-view-spec.js
@@ -3,6 +3,9 @@
 import EmojiKeywordView from '../lib/custom-elements/emoji-keyword-view';
 import { getLanguage } from '../lib/helpers/get-all-languages';
 
+/**
+ * @todo Add functional testing. Right now there is only basic DOM testing.
+ */
 describe('<emoji-keyword>', () => {
    let exampleLanguage = getLanguage('javascript').all;
    let element = new EmojiKeywordView();
@@ -16,10 +19,28 @@ describe('<emoji-keyword>', () => {
       });
    });
 
-   it('has all DOM elements available', () => {
+   it('is correctly created and has all data-attributes', () => {
       expect(element).toBeDefined();
-      expect(element.querySelector('h3 code')).toBeDefined();
+
+      expect(element.hasAttribute('data-emoji-language')).toBe(true);
+      expect(element.getAttribute('data-emoji-language').length).toBeGreaterThan(0);
+
+      expect(element.hasAttribute('data-emoji-keyword')).toBe(true);
+      expect(element.getAttribute('data-emoji-keyword').length).toBeGreaterThan(0);
+   });
+
+   it('has a headline with content', () => {
+      let headline = element.querySelector('h3 code');
+      expect(headline).toBeDefined();
+      expect(headline.textContent.length).toBeGreaterThan(0);
+      expect(headline.textContent).not.toBeUndefined();
+   });
+
+   it('contains the options wrapper', () => {
       expect(element.querySelector('.emoji-options')).toBeDefined();
+   });
+
+   it('has all options', () => {
       expect(element.querySelector('.option-activate')).toBeDefined();
       expect(element.querySelector('.option-emoij')).toBeDefined();
       expect(element.querySelector('.option-pseudo')).toBeDefined();
