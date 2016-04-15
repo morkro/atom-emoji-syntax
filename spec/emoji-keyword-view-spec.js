775c30d0beb464f891efab9d91b3539537c34990
Moritz Kröger <write@morkro.de>
2016-04-15 14:15:08 +0200

    Added ESLint and updated all files

--- spec/emoji-keyword-view-spec.js
+++ spec/emoji-keyword-view-spec.js
@@ -7,43 +7,43 @@ import { getLanguage } from '../lib/helpers/get-all-languages';
  * @todo Add functional testing. Right now there is only basic DOM testing.
  */
 describe('<emoji-keyword>', () => {
-   let exampleLanguage = getLanguage('javascript').all;
-   let element = new EmojiKeywordView();
+	const exampleLanguage = getLanguage('javascript').all;
+	const element = new EmojiKeywordView();
 
-   beforeEach(() => {
-      element.initialize({
-         language: exampleLanguage.languageName,
-         selector: exampleLanguage.languageSelector,
-         keyword: 'import',
-         content: exampleLanguage.import
-      });
-   });
+	beforeEach(() => {
+		element.initialize({
+			language: exampleLanguage.languageName,
+			selector: exampleLanguage.languageSelector,
+			keyword: 'import',
+			content: exampleLanguage.import
+		});
+	});
 
-   it('is correctly created and has all data-attributes', () => {
-      expect(element).toBeDefined();
+	it('is correctly created and has all data-attributes', () => {
+		expect(element).toBeDefined();
 
-      expect(element.hasAttribute('data-emoji-language')).toBe(true);
-      expect(element.getAttribute('data-emoji-language').length).toBeGreaterThan(0);
+		expect(element.hasAttribute('data-emoji-language')).toBe(true);
+		expect(element.getAttribute('data-emoji-language').length).toBeGreaterThan(0);
 
-      expect(element.hasAttribute('data-emoji-keyword')).toBe(true);
-      expect(element.getAttribute('data-emoji-keyword').length).toBeGreaterThan(0);
-   });
+		expect(element.hasAttribute('data-emoji-keyword')).toBe(true);
+		expect(element.getAttribute('data-emoji-keyword').length).toBeGreaterThan(0);
+	});
 
-   it('has a headline with content', () => {
-      let headline = element.querySelector('h3 code');
-      expect(headline).toBeDefined();
-      expect(headline.textContent.length).toBeGreaterThan(0);
-      expect(headline.textContent).not.toBeUndefined();
-   });
+	it('has a headline with content', () => {
+		const headline = element.querySelector('h3 code');
+		expect(headline).toBeDefined();
+		expect(headline.textContent.length).toBeGreaterThan(0);
+		expect(headline.textContent).not.toBeUndefined();
+	});
 
-   it('contains the options wrapper', () => {
-      expect(element.querySelector('.emoji-options')).toBeDefined();
-   });
+	it('contains the options wrapper', () => {
+		expect(element.querySelector('.emoji-options')).toBeDefined();
+	});
 
-   it('has all options', () => {
-      expect(element.querySelector('.option-activate')).toBeDefined();
-      expect(element.querySelector('.option-emoij')).toBeDefined();
-      expect(element.querySelector('.option-pseudo')).toBeDefined();
-      expect(element.querySelector('.option-spacing')).toBeDefined();
-   });
+	it('has all options', () => {
+		expect(element.querySelector('.option-activate')).toBeDefined();
+		expect(element.querySelector('.option-emoij')).toBeDefined();
+		expect(element.querySelector('.option-pseudo')).toBeDefined();
+		expect(element.querySelector('.option-spacing')).toBeDefined();
+	});
 });
--- spec/emoji-syntax-spec.js
+++ spec/emoji-syntax-spec.js
@@ -1,25 +1,24 @@
 'use babel';
 
 describe('Emoji Syntax', () => {
-   let atomStylesElement = null;
-   let activationPromise = null;
+	let atomStylesElement = null;
 
-   beforeEach(() => {
-      atomStylesElement = atom.stylesElement;
-      activationPromise = atom.packages.activatePackage('emoji-syntax');
-   });
+	beforeEach(() => {
+		atomStylesElement = atom.stylesElement;
+		atom.packages.activatePackage('emoji-syntax');
+	});
 
-   describe('when the package is activated', () => {
-      it('should add an <style> element to the DOM', () => {
-         expect(atomStylesElement.querySelector('.emoji-syntax-stylesheet')).toBeDefined();
-      })
-   });
+	describe('when the package is activated', () => {
+		it('should add an <style> element to the DOM', () => {
+			expect(atomStylesElement.querySelector('.emoji-syntax-stylesheet')).toBeDefined();
+		});
+	});
 
-   describe('when the package is deactivated', () => {
-      beforeEach(() => atom.packages.deactivatePackage('emoji-syntax'));
+	describe('when the package is deactivated', () => {
+		beforeEach(() => atom.packages.deactivatePackage('emoji-syntax'));
 
-      it('should remove the <style> element from the DOM', () => {
-         expect(atomStylesElement.querySelector('.emoji-syntax-stylesheet')).toBeNull();
-      })
-   });
+		it('should remove the <style> element from the DOM', () => {
+			expect(atomStylesElement.querySelector('.emoji-syntax-stylesheet')).toBeNull();
+		});
+	});
 });
