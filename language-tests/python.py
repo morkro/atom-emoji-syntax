3b614c432ee808140e2ed85cc7629d8649157855
Moritz Kröger <write@morkro.de>
2017-01-15 15:35:36 +0100

    Added more Python keywords

--- language-tests/python.py
+++ language-tests/python.py
@@ -7,3 +7,32 @@ import keyword
 # Keyword: global
 ##
 global foo
+
+##
+# Keyword: def
+##
+def improper_return_function(a):
+    if (a % 2) == 0:
+        return True
+
+##
+# Keyword: class
+##
+class ExampleClass:
+    def function1(parameters):
+        …
+    def function2(parameters):
+        …
+
+##
+# Keyword: nonlocal
+##
+nonlocal foo
+def outer_function():
+    a = 5
+    def inner_function():
+        nonlocal a
+        a = 10
+        print("Inner function: ",a)
+    inner_function()
+    print("Outer function: ",a)
